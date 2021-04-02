14815b7c6d4902efa69936d496553280cb182373
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-04-01 22:02:19 -0400

    created an object containing quiz questions and answers

---
+++ assets/js/quiz.js
@@ -0,0 +1,27 @@
+var quiz = [
+{
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: ["<javascript>", "<js>", "<script>", "<scripting>"],
+    answer: "<script>"
+}
+{
+    question: "How do you write \"Hello World\" in an alert box?",
+    options: ["alert(\"Hello World\")", "msgBox(\"Hello World\")", "alertBox=\"Hello World\"", "alertBox(\"Hello World\")"],
+    answer: "alert(\"Hello World\")"
+}
+{
+    question: "How do you create a function?",
+    options: ["function:myFunction()", "function=myFunction()", "function myFunction()", "myFunction():function"],
+    answer: "function myFunction()"
+}
+{
+    question: "How do you call a function named \"myFunction\"?",
+    options: ["call myFunction()", "myFunction()", "call function myFunction", "Call.myFunction()"],
+    answer: "myFunction()"
+}
+{
+    question: "How do you write a conditional statement for executing some statements only if \"i\" is equal to 5?",
+    options: ["if i==5 then", "if (i==5)", "if i=5 then", "if i=5"],
+    answer: "if (i==5)"
+}
+]
